1023465638eda6e2c37cdb583c8fd8f555dce805
Chanuka Vithanage <144988968+chanukav@users.noreply.github.com>
2026-07-04 23:13:31 +0530

    feat: initialize configuration file with API base URL environment variable support

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL?.toString() || 'http://localhost:8080'
+  import.meta.env.VITE_API_BASE_URL?.toString() ??
+  (import.meta.env.PROD ? '' : 'http://localhost:8080')
 
